9a3d053a8f1d2811c6c8d95129bd3c9a611dd9f2
ayelen027 <ayelenveronicaalegre@gmail.com>
2023-03-19 19:24:49 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
